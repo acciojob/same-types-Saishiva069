07eb0776ea9d175d257d51a3dcefd5cf9fedd304
Saishiva069 <vankadothsaishiva444@gmail.com>
2026-05-23 02:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function isSameType(value1, value2) {
+
   // Check if both are NaN
-  if (isNaN(value1) && isNaN(value2)) {
+  if (Number.isNaN(value1) && Number.isNaN(value2)) {
     return true;
+  }
+
+  // If only one is NaN
+  if (Number.isNaN(value1) || Number.isNaN(value2)) {
+    return false;
   }
 
   // Compare types
@@ -11,4 +17,5 @@ function isSameType(value1, value2) {
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
+
 alert(isSameType(value1, value2));
